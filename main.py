8416c41c30b79bd0d20bd0588b7a58de5d22dd96
gab-in <gab.covalski@gmail.com>
2025-12-12 20:27:41 -0300

    Fix de tamanho de fonte

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def blocoQrCode(preview,qrCode):
 
     previewQrCode=escalaFonte(preview)
     previewQrCode.color="#AA77C7"
-    previewQrCode.font_size=26
+    previewQrCode.font_size=30
     previewQrCode.next_to(qrCode,UP)
 
     return Group(qrCode,previewQrCode)
@@ -50,7 +50,7 @@ class main(Scene):
         # Os nomes e cargos em si
         titulo=escalaFonte("Creditos")
         titulo.color="#AA77C7"
-        titulo.font_size=26
+        titulo.font_size=30
 
         blocos=[ 
                  blocoCargo("Diretor","Alguém Ai"),
