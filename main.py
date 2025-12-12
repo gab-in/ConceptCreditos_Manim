b210fcdb0e6ce3ce015466443684603c6091e059
gab-in <gab.covalski@gmail.com>
2025-12-12 15:00:58 -0300

    V1

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ def blocoCargo(cargo,nome):
 
     return Group(cargoText,nomeText)
 
-def blocoQrCode(preview,qrCode):
-    qrCode.scale(0.2)
-    previewQrCode=Text(preview,font_size=20)
-    previewQrCode.next_to(qrCode,UP)
-
-    return Group(qrCode,previewQrCode)
-
 class main(Scene):
     def construct(self):
+        # ------------------------- CONFIGURAÇÕES? -------------------------
+        self.camera.background_color="#1E1E1E"
+        
         # ------------------------- OBJETOS ---------------------------
         # Os nomes e cargos em si
         titulo=Tex("Creditos")
@@ -35,27 +31,57 @@ class main(Scene):
             creditos.add(bloco)
             dist=bloco
             
-        # Os QrCodes para acesso às outras plataformas
-        blocos=[
-            blocoQrCode("Youtube", ImageMobject("/home/autumn/Imagens/Never1.png")),
-            blocoQrCode("Moodle", ImageMobject("/home/autumn/Imagens/Never1.png")),
-            blocoQrCode("Discord", ImageMobject("/home/autumn/Imagens/Never1.png"))
-        ]
+        # MObjetos animados como menu ps2
+        triangle = Triangle(color="#e07a5f", fill_opacity=1).move_to(RIGHT*6+UP*2)
+        circle = Circle(color="#87c2a5", fill_opacity=1).move_to(UP*3)
+        square = Square(color="#525893", fill_opacity=1).move_to(DOWN*2) #LEFT*6+DOWN*2
+        #.scale(0.4)
 
-        qrCodes=Group()
-        dist=blocos[0]
-        for bloco in blocos:
-            bloco.move_to(dist.get_bottom()+DOWN*1.5)
-            qrCodes.add(bloco)
-            dist=bloco
+        # Aqui as rotas que o MObjects vão tomar
+        rota1= Circle(radius=1.5).shift(RIGHT*3)
+        rota2 = rota1.copy().rotate(2*PI/3)
+        rota3= rota1.copy().rotate(4*PI/3)
 
         # ------------------------ ANIMAÇÕES ---------------------
         self.add(creditos.move_to(DOWN*10))
         self.play(creditos.animate.move_to(ORIGIN),run_time=2.5)
 
+        # Nome+Cargos vai para a direita e FadeIn() dos MObjects 
+        self.play(AnimationGroup(
+            creditos.animate.move_to(LEFT*3), 
+            FadeIn(triangle.scale(0.4)),
+            triangle.animate.move_to(rota1.point_from_proportion(0)),
+            run_time=2
+        ))
+        
+        # Rotaciona o Triangulo e da FadeIn() no círculo
+        self.play(AnimationGroup(
+            MoveAlongPath(triangle, rota1), 
+            FadeIn(circle.scale(0.4)),
+            circle.animate.move_to(rota1.point_from_proportion(1/3)),
+            run_time=2.5,
+            rate_func=linear
+        ))
 
-        # Aqui tem que acontece o FadeIn() dos QR Codes ao mesmo tempo 
-        self.play(creditos.animate.move_to(RIGHT*4), FadeIn(qrCodes.move_to(LEFT*4)), run_time=2)
+        # Rotaciona Triangulo e Circulo e da FadeIn() no quadrado
+        self.play(AnimationGroup(
+            MoveAlongPath(triangle,rota1),
+            MoveAlongPath(circle,rota2),
+            FadeIn(square.scale(0.4)),
+            square.animate.move_to(rota1.point_from_proportion(2/3)),
+            run_time=2.5,
+            rate_func=linear
+        ))
+
+        # Rotaciona Triangulo, Circulo e Quadrado
+        for i in range(3):
+            self.play(AnimationGroup(
+                MoveAlongPath(triangle,rota1),
+                MoveAlongPath(circle,rota2),
+                MoveAlongPath(square,rota3),
+                run_time=2.5,
+                rate_func=linear
+            ))
 
         
 
